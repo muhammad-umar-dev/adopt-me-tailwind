0287f3040d504e9c4d0c0fbf4cac1f2f664454e2
muhammad-umar-dev <umar.osamafarooq@gmail.com>
2023-02-04 11:50:23 +0500

    tailwind configure3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["'./src/**/*.{html,js,ts,jsx,tsx}','./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}  "],
   theme: {
     fontFamily: {
       body: ["Segoe UI", "Tahoma", "Geneva", "Verdana", "sans-serif"],
